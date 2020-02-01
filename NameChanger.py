f6aa59c7fb7a3d22db0be40cb2791aa1674ee54c
Filipe <eminem100@hotmail.com>
2020-02-01 13:03:10 +0000

    Script version 1.0

--- NameChanger.py
+++ NameChanger.py
@@ -1,8 +1,29 @@
 import sys
+from os import listdir, path, rename
+from os.path import isfile, join
+from random import randrange
 
 
 def main(folderName):
-    print(folderName)
+    if not path.exists(folderName):
+        print("The folder " + str(folderName) + " was not found.")
+        return
+
+    # Process the files in the folder
+    files = [f for f in listdir(folderName) if isfile(join(folderName, f))]
+
+    print("Found " + str(len(files)) + " files in the folder.")
+    for f in files:
+        changeName(folderName, f)
+    pass
+
+
+def changeName(folderName, fileName):
+    suffix = randrange(0, 1000000)
+
+    oldFile = folderName + "/" + fileName
+    newFile = folderName + "/" + str(suffix) + "_" + fileName
+    rename(oldFile, newFile)
     pass
 
 
